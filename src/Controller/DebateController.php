0187a7f41f5f2dbe242d77488a435c1cc9567871
gtrb <guillaume.trb@hotmail.com>
2019-05-28 20:53:02 +0200

    created service to reformat dates

--- src/Controller/DebateController.php
+++ src/Controller/DebateController.php
@@ -2,22 +2,37 @@
 namespace App\Controller;
 
 use App\Entity\Debate;
+use App\Service\Relativetime;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class DebateController extends Controller {
+class DebateController extends AbstractController {
+  
   /**
-  * @Route("/")
+  * @Route("/", name="debate_list")
   * @Method("GET")
   */
-
-  public function index(){
+  public function index(Relativetime $relativetime){
+    /**
+     * TODO: Changer la date écrite en dur par la date du debat & faire la même chose pour la page show
+      */
+    $processed = $relativetime->time_elapsed_string('2010-04-28 17:25:43');
+    \var_dump($processed);
 
     $debates = $this->getDoctrine()->getRepository(Debate::class)->findAll();
-
     return $this->render('debates/index.html.twig', array('debates'=>$debates)); 
+  }
+  
+  /**
+  * @Route("/debate/{id}", name="debate_show")
+  * @Method("GET")
+  */
+  public function show($id){
+    $debate = $this->getDoctrine()->getRepository(Debate::class)->find($id);
+
+    return $this->render('debates/show.html.twig', array('debate'=>$debate)); 
   }
 }
---
+++ src/Service/Relativetime.php
@@ -0,0 +1,35 @@
+<?php
+namespace App\Service;
+
+class Relativetime
+{
+  
+  public function time_elapsed_string($datetime, $full = false) {
+    $now = date_create("now");
+    $ago = date_create($datetime);
+    $diff = $now->diff($ago);
+
+    $diff->w = floor($diff->d / 7);
+    $diff->d -= $diff->w * 7;
+
+    $string = array(
+        'y' => 'year',
+        'm' => 'month',
+        'w' => 'week',
+        'd' => 'day',
+        'h' => 'hour',
+        'i' => 'minute',
+        's' => 'second',
+    );
+    foreach ($string as $k => &$v) {
+        if ($diff->$k) {
+            $v = $diff->$k . ' ' . $v . ($diff->$k > 1 ? 's' : '');
+        } else {
+            unset($string[$k]);
+        }
+    }
+
+    if (!$full) $string = array_slice($string, 0, 1);
+    return $string ? implode(', ', $string) . ' ago' : 'just now';
+  } 
+}
