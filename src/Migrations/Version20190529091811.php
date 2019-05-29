eea6eda926fe4385acf559f0ea78c169023c4442
edwinvautier <edwin.vautier@gmail.com>
2019-05-29 11:21:19 +0200

    initial commit

--- src/Migrations/Version20190529091811.php
+++ src/Migrations/Version20190529091811.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190528221319 extends AbstractMigration
+final class Version20190529091811 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -22,7 +22,8 @@ final class Version20190528221319 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE debate (id INT AUTO_INCREMENT NOT NULL, title TINYTEXT NOT NULL, content LONGTEXT NOT NULL, category TINYTEXT NOT NULL, author TINYTEXT NOT NULL, created DATETIME NOT NULL, side1 TINYTEXT NOT NULL, side2 TINYTEXT NOT NULL, side1_votes INT NOT NULL, side2_votes INT NOT NULL, total_votes INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE comment (id INT AUTO_INCREMENT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE debate (id INT AUTO_INCREMENT NOT NULL, title TINYTEXT NOT NULL, description LONGTEXT NOT NULL, category TINYTEXT NOT NULL, author TINYTEXT NOT NULL, created DATETIME NOT NULL, side1 TINYTEXT NOT NULL, side2 TINYTEXT NOT NULL, side1_votes INT NOT NULL, side2_votes INT NOT NULL, total_votes INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
     }
 
     public function down(Schema $schema) : void
@@ -30,6 +31,7 @@ final class Version20190528221319 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
+        $this->addSql('DROP TABLE comment');
         $this->addSql('DROP TABLE debate');
     }
 }
