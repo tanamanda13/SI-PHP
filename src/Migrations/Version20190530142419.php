76e8c6174bcf22a8521285ee5c532a998fe2b212
gtrb <guillaume.trb@hotmail.com>
2019-05-30 17:56:41 +0200

    hid the buttons on navbar when on login or register page and added redirection from those pages when user is connected

--- src/Migrations/Version20190530142419.php
+++ src/Migrations/Version20190530142419.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190530112332 extends AbstractMigration
+final class Version20190530142419 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -25,7 +25,6 @@ final class Version20190530112332 extends AbstractMigration
         $this->addSql('CREATE TABLE vote (id INT AUTO_INCREMENT NOT NULL, author_id INT NOT NULL, debate_id INT NOT NULL, UNIQUE INDEX UNIQ_5A108564F675F31B (author_id), UNIQUE INDEX UNIQ_5A10856439A6B6F6 (debate_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('ALTER TABLE vote ADD CONSTRAINT FK_5A108564F675F31B FOREIGN KEY (author_id) REFERENCES user (id)');
         $this->addSql('ALTER TABLE vote ADD CONSTRAINT FK_5A10856439A6B6F6 FOREIGN KEY (debate_id) REFERENCES debate (id)');
-        $this->addSql('DROP TABLE rememberme_token');
     }
 
     public function down(Schema $schema) : void
@@ -33,7 +32,6 @@ final class Version20190530112332 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE rememberme_token (series CHAR(88) NOT NULL COLLATE utf8mb4_general_ci, value CHAR(88) NOT NULL COLLATE utf8mb4_general_ci, lastUsed DATETIME NOT NULL, class VARCHAR(100) NOT NULL COLLATE utf8mb4_general_ci, username VARCHAR(200) NOT NULL COLLATE utf8mb4_general_ci, UNIQUE INDEX series (series), PRIMARY KEY(series)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB COMMENT = \'\' ');
         $this->addSql('DROP TABLE vote');
     }
 }
